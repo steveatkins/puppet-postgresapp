29347eea9ab9ec7b591f71c06e053a975cbe43f7
Steve Atkins <steve@blighty.com>
2014-09-16 16:09:11 -0700

    Correct name for app

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include postgresapp
 
 class postgresapp {
-  package { 'Postgresapp':
+  package { 'Postgres':
     source   => 'https://github.com/PostgresApp/PostgresApp/releases/download/9.3.5.0/Postgres-9.3.5.0.zip',
     provider => 'compressed_app',
   }
